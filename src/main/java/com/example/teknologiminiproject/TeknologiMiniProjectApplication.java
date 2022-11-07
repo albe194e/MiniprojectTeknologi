24a2652aecde0721e8efb18219e35734b8463438
albe194e <albe194e@stud.kea.dk>
2022-11-07 15:06:37 +0100

    First

---
+++ src/main/java/com/example/teknologiminiproject/TeknologiMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.teknologiminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiMiniProjectApplication.class, args);
+    }
+
+}
